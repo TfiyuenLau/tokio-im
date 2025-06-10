53bddcf63500bcbeb1b12ce33bdbb1081da5b705
TfiyuenLau <tfiyuenlau@foxmail.com>
2025-06-10 22:10:29 +0800

    Streamlined operations.

--- src/service/user_service.rs
+++ src/service/user_service.rs
@@ -5,9 +5,9 @@ use std::collections::HashMap;
 pub async fn login(user: User) -> Option<User> {
     // 模拟用户数据
     let mut users: HashMap<String, String> = HashMap::new();
-    users.insert("zhangsan".to_string(), "123456".to_string());
-    users.insert("lisi".to_string(), "123456".to_string());
-    users.insert("wangwu".to_string(), "123456".to_string());
+    users.insert("zhangsan".to_string(), "123".to_string());
+    users.insert("lisi".to_string(), "123".to_string());
+    users.insert("wangwu".to_string(), "123".to_string());
 
     // 登录逻辑实现
     if !users.contains_key(&user.username) || users.get(&user.username).unwrap() != &user.password {
